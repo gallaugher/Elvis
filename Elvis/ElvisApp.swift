fa247b447b94d2c81f93045e053b5c5be79b0c37
gallaugher <john.gallaugher@bc.edu>
2025-01-08 13:07:26 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by John Gallaugher on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
